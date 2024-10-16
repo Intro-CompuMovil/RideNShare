a181c6a3160cc3f920678ca101f9685c052e6963
alejo190404 <alejo190404@gmail.com>
2024-10-16 18:00:27 -0500

    Parte alejo

--- app/src/main/java/com/example/ridenshare/Logica/MainActivity.kt
+++ app/src/main/java/com/example/ridenshare/Logica/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.ridenshare.Logica
 
 import android.content.Intent
+import androidx.biometric.BiometricPrompt;
+import androidx.biometric.BiometricPrompt.PromptInfo;
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.ridenshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
